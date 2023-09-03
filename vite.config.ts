0acd1159e6d897d5deecc44609b40935206bf13a
Matheus Nadaleti Garcia <mat.nadaleti@hotmail.com>
2023-09-03 16:04:52 -0300

    Configuring workflow to deploy static page on gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/quartz-calculator",
+  base: "/quartz-calculator/",
   plugins: [svelte()],
 })
